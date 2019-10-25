6003a409ae638f3b4f7bc725962aa1b34e23863c
kuwingto <kuwingto>
2019-10-25 17:07:28 +0800

    exclude redundant files from project

--- src/winmain.cpp
+++ src/winmain.cpp
@@ -1,12 +1,34 @@
 #include <string>
 
+
+
+extern int gpucs3(std::string scene_path, std::string object_path, std::string ppf_path);
+extern int preprocess2(std::string model_path);
+int main(int argc, char** argv)
+{
+	int mode = 1;
+
+	if (mode == 0)
+	{
+		std::string modelpath = "D:/ronaldwork/model_matching/customtestcases/1/tmodel.ply";
+		preprocess2(modelpath);
+	}
+	else
+	{
+		std::string modelpath = "./model_search.ply";
+		std::string ppfpath = "./ppf_map";
+		std::string scenePath = "D:/ronaldwork/model_matching/customtestcases/1/";
+		gpucs3(scenePath, modelpath, ppfpath);
+	}
+}
+
+/*
 extern int preprocess(std::string object_name);
 extern int stocs_est(std::string scene_path, std::string object_name);
 extern int testBruteforceReg(std::string scene_path, std::string object_name);
 extern int gpucs(std::string scene_path, std::string object_name);
 extern int gpucs2(std::string scene_path, std::string object_name);
-extern int gpucs3(std::string scene_path, std::string object_path, std::string ppf_path);
-extern int preprocess2(std::string model_path);
+
 
 int oldapi()
 {
@@ -37,21 +59,4 @@ int oldapi()
 
 	return 0;
 }
-
-int main(int argc, char** argv)
-{
-	int mode = 1;
-
-	if (mode == 0)
-	{
-		std::string modelpath = "D:/ronaldwork/model_matching/customtestcases/1/tmodel.ply";
-		preprocess2(modelpath);
-	}
-	else
-	{
-		std::string modelpath = "./model_search.ply";
-		std::string ppfpath = "./ppf_map";
-		std::string scenePath = "D:/ronaldwork/model_matching/customtestcases/1/";
-		gpucs3(scenePath, modelpath, ppfpath);
-	}
-}
+*/
